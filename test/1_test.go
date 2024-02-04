a19b6d0ff52476d4b31fcdb0ca98a6d1540f64d6
sssnak <sssnak@163.com>
2024-02-04 22:10:19 +0800

    回溯算法 2/3

---
+++ test/1_test.go
@@ -0,0 +1,61 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+)
+
+//回溯-1  LCR 083. 全排列   https://leetcode.cn/problems/VvJkup/solutions/2591310/jing-dian-hui-su-by-song-jia-liang-fzqj/
+
+func TestTimeConsuming(t *testing.T) {
+	nums := []int{1, 2, 3}
+	result1 := permute(nums)
+	fmt.Println(result1)
+}
+
+// 1 <= nums.length <= 6 // -10 <= nums[i] <= 10  // nums 中的所有整数 互不相同
+
+var result [][]int
+
+func permute(nums []int) [][]int {
+	track := make([]int, 0, 0)
+	result_ = make([][]int, 0, 0) //为了每次清空上一次的结果
+	backTrack(nums, track)
+	return result
+}
+
+func backTrack(nums []int, track []int) {
+	// list 暂存全排列的数组
+	if len(track) == len(nums) { //长度一样了，那就记录结果
+		//result = append(result, track) //有问题，为track是一个切片，后续的回溯过程会修改track，导致之前添加到result中的全排列也会被修改。这样会导致最终的结果不正确。
+		trackCopy := make([]int, len(track))
+		copy(trackCopy, track)
+		result = append(result, trackCopy)
+	}
+	for _, num := range nums {
+		//避免有重复的数字
+		if has(track, num) {
+			continue
+		}
+		track = append(track, num) //添加上路径节点
+		backTrack(nums, track)     //再往下递归
+		//del，这是回溯算法的关键，回溯
+		track = track[0 : len(track)-1]
+
+	}
+
+}
+
+func has(track []int, currrnt int) bool {
+	for _, value := range track {
+		if value == currrnt {
+			return true
+		}
+	}
+	return false
+}
+
+func Test2(t *testing.T) {
+	nums := []int{1, 2, 3, 4, 5}
+	fmt.Println(nums[0 : len(nums)-1])
+}
---
+++ test/组合_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+)
+
+// 回溯-2  77. 组合  https://leetcode.cn/problems/combinations/description/
+func TestName(t *testing.T) {
+	i := combine(1, 1)
+	fmt.Println(i)
+}
+
+var result_ [][]int
+
+func combine(n int, k int) [][]int {
+	track := make([]int, 0, 0)
+	//result_ := make([][]int, 0, 0) // fix 初始化
+	backtrace(n, k, 1, track)
+	return result_
+}
+
+func backtrace(n, k, start int, track []int) {
+	//	从1到n，取k个
+	if len(track) == k {
+		trackCopy := make([]int, len(track))
+		copy(trackCopy, track)
+		result_ = append(result_, trackCopy)
+	}
+	for i := start; i <= n; i++ { //start=1 是因为值的范围是[1,n]
+		track = append(track, i)        //add
+		backtrace(n, k, i+1, track)     //递归，i+1是因为i已经包含了，不能重复
+		track = track[0 : len(track)-1] //回溯
+	}
+}
